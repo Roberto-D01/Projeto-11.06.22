2ca75001231e86f2d9ebba139c9ff74e0d5acef3
Roberto Carlos <74996793+Roberto-D01@users.noreply.github.com>
2022-06-11 18:09:03 -0300

    Começo

---
+++ script.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login__button');
+
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
+}
+
+
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+inputs.forEach((input) => input.addEventListener('input', handleChange));
+
